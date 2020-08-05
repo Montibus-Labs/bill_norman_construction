ca1902c925c4241b52ffd1b49b80d451e2acb732
Cody Norman <wcodynorman@gmail.com>
2020-08-05 08:41:43 -0600

    update colors to white

--- src/subpages/End.js
+++ src/subpages/End.js
@@ -13,35 +13,30 @@ const contactButtonStyles = {
 
 export default function End() {
   return (
-    <div
-      className="container-fluid"
-      style={{ backgroundColor: "gray", color: "white" }}
-    >
+    <div className="container-fluid">
       <div className="container">
         <div className="flexbox">
-          <Fade left>
-            <h1 className="text-center my-5">Stay in touch</h1>
-            <div className="row my-4">
-              <div className="col-lg-6 mb-4">
-                <input
-                  type="name"
-                  className="inputBox"
-                  placeholder="Your Name"
-                />
-              </div>
-              <div className="col-lg-6 mb-4">
-                <input
-                  type="email"
-                  className="inputBox"
-                  placeholder="Your Email"
-                />
-              </div>
+          <h1 className="text-center my-5">Stay in touch</h1>
+          <div className="row my-4">
+            <div className="col-lg-6 mb-4">
+              <input
+                type="name"
+                className="inputBox"
+                placeholder="Your Name"
+              />
             </div>
-            <textarea rows = "2" className="inputBox mb-4" placeholder="Your Message" />
-            <button className="btn mb-4 py-3" style={{ backgroundColor: 'red', color: 'white' }}>
-              Submit
-            </button>
-          </Fade>
+            <div className="col-lg-6 mb-4">
+              <input
+                type="email"
+                className="inputBox"
+                placeholder="Your Email"
+              />
+            </div>
+          </div>
+          <textarea rows = "2" className="inputBox mb-4" placeholder="Your Message" />
+          <button className="btn mb-4 py-3" style={{ backgroundColor: 'red', color: 'white' }}>
+            Submit
+          </button>
         </div>
       </div>
     </div>
--- src/subpages/Team.js
+++ src/subpages/Team.js
@@ -16,11 +16,11 @@ function Team() {
     }
   `)
   return (
-    <div id = "team" className="container-fluid" style={{ backgroundColor: "#AFECA4" }}>
+    <div id = "team" className="container-fluid">
       <div className="container">
         <div className="flexbox">
           <h1 className="text-center my-4">Why Choose Us?</h1>
-          {/* please dont use a header for this, just getting a size idea */}
+           {/* please dont use a header for this, just getting a size idea */}
           <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
           {/* <div className="row my-5"> */}
           {/*   {allTeamYaml.edges.map(({ node }) => { */}
