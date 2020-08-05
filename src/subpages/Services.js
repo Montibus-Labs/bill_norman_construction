7681e2fd4b839f99801bc30a5e39afc2d7969835
Cody Norman <wcodynorman@gmail.com>
2020-08-04 21:00:59 -0600

    remove service description

--- src/subpages/Services.js
+++ src/subpages/Services.js
@@ -29,22 +29,18 @@ function Services() {
             <div className="col-lg-3 my-4 col-sm-12 text-center">
               <FontAwesomeIcon icon={faHome} size="8x" />
               <h3 className="mt-5">New Construction</h3>
-              <p>Here is some stuff a bout building a house</p>
             </div>
             <div className="col-lg-3 my-4 col-sm-12 text-center">
               <FontAwesomeIcon icon={faPaintRoller} size="8x" />
               <h3 className="mt-5">Remodels</h3>
-              <p>Here is some stuff a bout building a house</p>
             </div>
             <div className="col-lg-3 my-4 col-sm-12 text-center">
               <FontAwesomeIcon icon={faTools} size="8x" />
               <h3 className="mt-5">Additions</h3>
-              <p>Here is some stuff a bout building a house</p>
             </div>
             <div className="col-lg-3 my-4 col-sm-12 text-center">
               <FontAwesomeIcon icon={faTint} size="8x" />
               <h3 className="mt-5">Roofing</h3>
-              <p>Here is some stuff a bout building a house</p>
             </div>
           </div>
         </div>
