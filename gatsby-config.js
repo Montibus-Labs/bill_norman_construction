e0ec5d32043d9b5a31ea4de23ed081cae1bbc6e8
Cody Norman <wcodynorman@gmail.com>
2020-08-06 09:59:29 -0600

    added simple favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-krisp`,
-        short_name: `krisp`,
+        name: `bill-norman-construction`,
+        short_name: `bill-norman-contstruction`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/bnc-favicon.png`,
       },
     },
     `gatsby-transformer-yaml`,
