92a2eefd172b07764ec0f7c3a767d3af822d629b
Cody Norman <wcodynorman@gmail.com>
2020-08-18 13:54:44 -0600

    fix typos

--- src/subpages/Team.js
+++ src/subpages/Team.js
@@ -22,8 +22,8 @@ function Team() {
           <h1 className="text-center my-4">Why Choose Us?</h1>
            {/* please dont use a header for this, just getting a size idea */}
           <div class="text-center" style={{ fontSize: "1.5em" }}>
-            <p>With more than 20 years experience, we provide the only the highest quality craftsmanship.  We take great pride in our work and will stand behind it 100%.  We treat your home or business as if it were our own.</p>
-            <p>Our residental and commercial services include new constructon, fire resotrations, roofing and emergency roofing repairs. Complete remodeling for residental and commercial</p>
+            <p>With more than 20 years experience, we provide only the highest quality craftsmanship.  We take great pride in our work and will stand behind it 100%.  We treat your home or business as if it were our own.</p>
+            <p>Our residental and commercial services include new construction, fire resotrations, roofing and emergency roofing repairs. Complete remodeling for residental and commercial</p>
             <ul style={{ listStyle: "none" }}>
               <li>Kitchens</li>
               <li>Bathrooms</li>
